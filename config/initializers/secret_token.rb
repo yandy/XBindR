57fdca156bcce1465cdb2af678ee79a864e1c2ad
Michael Ding <dingyan@freestorm.org>
2012-11-12 19:49:48 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xbindr::Application.config.secret_token = 'ef836e6f125702eb5dd9a98cf4b56f16c7fb7fffc89ed3fa6b9288a3a782b02b18777f1e52b7c391ba8c095237bc10ff38a48f20913d68922f064213a6ceb311'
