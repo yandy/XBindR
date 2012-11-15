ef93b929985bd3bd08a7dca78017aa015d6c64f7
Michael Ding <dingyan@freestorm.org>
2012-11-15 22:59:30 +0800

    build app layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Xbindr::Application.routes.draw do
 
   resource :data, controller: :data, only: [:show, :create]
 
+  get "help" => "site#help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +56,7 @@ Xbindr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'site#index'
 
   # See how all your routes lay out with "rake routes"
 
