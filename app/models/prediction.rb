e4af4b783d52dc29553f4fba5f9c138b6fd40977
Michael Ding <dingyan@freestorm.org>
2013-01-15 19:47:51 +0800

    add prediction form

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,3 +1,3 @@
 class Prediction < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :res_arr
 end
---
+++ app/workers/predict.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+class Predict
+  @queue = :data_parser
+
+  def self.perform
+  end
+end
