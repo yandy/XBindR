23e0d49dcc05d7e2475ba9fdaa70b7f285d5b5a6
Michael Ding <yandy.ding@gmail.com>
2013-01-25 15:30:56 +0800

    complete prediction new

---
+++ app/mailers/result_mailer.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+class ResultMailer < ActionMailer::Base
+
+  include Resque::Mailer
+
+  default from: "XBindR <no-reply@cbi.seu.edu.cn"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.inform_mailer.send_notice.subject
+  #
+  def binding_prediction_mail(pred, emails)
+    @pred = pred
+
+    mail ({to: emails, subject: "[XBindR]Binding Site Prediction Result, sid: #{pred["id"]}"})
+  end
+end
