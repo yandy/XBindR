4349e390a78ad1cfc0c60a87ed90bf53bbd9db82
Michael Ding <yandy.ding@gmail.com>
2013-03-04 19:32:33 +0800

    use instance method instead of class method

--- lib/xbindr/dna_prediction.rb
+++ lib/xbindr/dna_prediction.rb
@@ -1,6 +1,23 @@
 module XbindR
 	class DNAPrediction
-		def self.do_predict
+		class self
+			def do_predict res_arr
+				p = DNAPrediction.new res_arr
+				p.predict_chain!
+				return p.res_status
+			end
+		end
+
+		attr_accessor :res_arr, :res_status
+
+		def initialize(res_arr)
+			self.res_arr = res_arr
+		end
+
+		def predict_chain!
+		end
+
+		def pssm
 			#pass
 		end
 	end
